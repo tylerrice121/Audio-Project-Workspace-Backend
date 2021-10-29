6cbf2a65a7c3b54477960c393c2595f189fa389b
Tyler Rice <tylerrice@Tylers-MBP-2.cable.rcn.com>
2021-10-29 09:59:38 -0500

    wip prepare for deployment

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const { PORT, DATABASE_URL } = process.env;
+const { PORT, DATABASE_URL, PRIVATE_KEY_ID, PRIVATE_KEY, CLIENT_ID } = process.env;
 const projectsController = require('./controllers/projects');
 const songsController = require('./controllers/songs');
 const admin = require("firebase-admin");
-const serviceAccount = require("./apw-auth-firebase-adminsdk-hjk2m-d9b696e8e2.json");
 
 // connect to mongodb
 
@@ -32,14 +31,28 @@ app.use(express.json());
 app.use('/api/songs', songsController);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    "type": "service_account",
+    "project_id": "apw-auth",
+    "private_key_id": PRIVATE_KEY_ID,
+    "private_key": PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": "firebase-adminsdk-hjk2m@apw-auth.iam.gserviceaccount.com",
+    "client_id": CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-hjk2m%40apw-auth.iam.gserviceaccount.com"
+  })
 });
 
 app.use(async function(req, res, next) {
     try {
         const token = req.get('Authorization');
-        const authUser = await admin.auth().verifyIdToken(token.replace('Bearer ', ''));
-        req.user = authUser;
+        if(token) {
+            const authUser = await admin.auth().verifyIdToken(token.replace('Bearer ', ''));
+            req.user = authUser;
+        }
+
         next();
     } catch (error) {
         console.log(error)
